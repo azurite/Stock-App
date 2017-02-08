1027f986ea7a7b5b301460f1853d1b4ac394b2be
Marko Nikic <darkblue.azurite@gmail.com>
2017-02-08 20:43:12 +0100

    add redis database

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const express = require("express");
 const app = express();
 
 const routes = require("./app/routes");
+const db = require("./app/redis");
 const configureSocket = require("./app/socket");
+
+const redis = require("redis");
+const client = redis.createClient();
 
 app.set("port", process.env.PORT || 8124);
 app.set("view engine", "pug");
@@ -12,12 +16,14 @@ app.set("views", path.join(process.cwd(), "client"));
 
 app.use(express.static(path.join(process.cwd(), "build", "client")));
 app.use(express.static(path.join(process.cwd(), "client")));
+
+app.use(db(client));
 app.use(routes());
 
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-configureSocket(io);
+configureSocket(io, client);
 
 server.listen(app.get("port"), () => {
   console.log("listening on port: " + app.get("port"));
